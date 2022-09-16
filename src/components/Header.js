d50d385102a58338e12c4e55fc10ba52b9749608
SallyMulupi <mulupisally2gmail.com>
2022-09-16 13:43:58 +0300

    set mode for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header({ isDarkMode, onDarkModeClick }) {
   return (
     <header>
-      <h2><span>Employee Records</span></h2>
+      <h2><span>Student Records</span></h2>
       <button onClick={onDarkModeClick}>
         {isDarkMode ? "Dark" : "Light"} Mode
       </button>
