7af016b28f993de7ffde0adf2734f95946a732c9
SallyMulupi <mulupisally2gmail.com>
2022-09-16 13:42:13 +0300

    add oncategorychange option value

---
+++ src/components/Filter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function Filter({ category, onCategoryChange }) {
+    return (
+      <div className="Filter">
+        <select
+          name="filter"
+          value={category}
+          onChange={(e) => onCategoryChange(e.target.value)}
+        >
+         <option value="Select Course">Select <Course></Course></option>
+          <option value="Finance">Finance</option>
+          <option value="Accounting">Accounting</option>
+          <option value="I.T">I.T</option>
+          <option value="I.C.T">I.C.T</option>
+          <option value="B.I.T">B.I.T</option>
+          <option value="Software Development">Software Development</option>
+          <option value="Data Science">Data Science</option>
+         
+        </select>
+      </div>
+    );
+  }
+  
+  export default Filter;
