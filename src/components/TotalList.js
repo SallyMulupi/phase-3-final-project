619ad7a990b66147c8a7a65f8dfa98d0f4389eee
SallyMulupi <mulupisally2gmail.com>
2022-09-16 13:52:58 +0300

    add delete function

--- src/components/TotalList.js
+++ src/components/TotalList.js
@@ -7,3 +7,16 @@ function TotalList() {
     const [selectedCategory, setSelectedCategory] = useState("All");
     const [students, setStudents] = useState([]);
   
+    useEffect(() => {
+        fetch("")
+          .then((r) => r.json())
+          .then((employees) => setStudents(students));
+      }, []);
+
+      function handleDeleteStudent(deletedStudent) {
+        const updatedStudents = students.filter(
+          (students) => students.id !== deletedStudent.id
+        );
+        setStudents(updatedStudents);
+      }
+    
