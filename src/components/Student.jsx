7ca13afa98fac4957b68256a6aa5b2e95d41199f
SallyMulupi <mulupisally2gmail.com>
2022-09-16 13:06:23 +0300

    add delete function to student

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -2,6 +2,20 @@ import React from "react";
 
 function Student({student, onDeleteStudent }){
     function handleDeleteClick(){
-        fetch
+        fetch(   {} , {
+            method: "DELETE",
+        })
+        .then((r) => r.json())
+        .then(() => onDeleteStudent(student));
     }
+
+    eturn (
+        <li className={employee.is_present ? "in-cart" : ""}>
+          <span>{employee.name}</span>
+          <span className="category">{employee.category}</span>
+          <button className="remove" onClick={handleDeleteClick}>
+            Delete
+          </button>
+        </li>
+      );
 }
