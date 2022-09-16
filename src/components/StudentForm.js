a7e838ac4e97db5be1fe4d7763565443029bb316
SallyMulupi <mulupisally2gmail.com>
2022-09-16 13:23:40 +0300

    add on add handle submit to studentform

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -2,4 +2,16 @@ import React, { useState } from "react";
 
 function StudentForm({ onAddStudent }) {
   const [name, setName] = useState("");
-  const [category, setCategory] = useState("Select course");
+  const [category, setCategory] = useState("Select Course");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName('');
+    
+    setCategory('Select Course')
+    const studentData = {
+      name: name,
+      category: category,
+      is_present: false,
+
+    };
